e5b27b7e3c82a147c349b6a5ca9e2fc66e187d63
Asanka Abeyweera <asanka.abeyweera@gmail.com>
2013-05-06 19:49:34 +0530

    Display speeds

--- projectile_2D.c
+++ projectile_2D.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <math.h>
+
 #include <GL/gl.h>
 #include <GL/glut.h>
 
@@ -20,6 +23,11 @@ void calculatPosition()
    yCoordinate = VELOCITY_Y * time - 0.5 * GRAVITY * time * time;
 }
 
+float getSpeed()
+{
+   return VELOCITY_Y - GRAVITY * time;
+}
+
 void reshape (int w, int h)
 {
    glViewport (0, 0, (GLsizei) w, (GLsizei) h);
@@ -29,16 +37,61 @@ void reshape (int w, int h)
    glMatrixMode (GL_MODELVIEW);
 }
 
+void setFontSize(float size)
+{
+   glScalef(size, size, size);
+}
+
+void printText(const unsigned char *string)
+{
+   glutStrokeString(GLUT_STROKE_ROMAN, string);
+}
+
+void printTextAt(int x, int y, const unsigned char *string)
+{
+   glPushMatrix();
+   {
+      glTranslatef(x, y, 0.0);
+      printText(string);
+   }
+   glPopMatrix();
+}
+
 void display()
 {
    glClear (GL_COLOR_BUFFER_BIT);
    glColor3f (1.0, 0.0, 0.0);
    glLoadIdentity ();             /* clear the matrix */
 
-   glTranslatef(xCoordinate, yCoordinate, 0.0);
-   glutWireSphere(20, 100, 2);   /* draw the ball */
+   glPushMatrix();
+   {
+      glTranslatef(xCoordinate, yCoordinate, 0.0);
+      glutWireSphere(20, 100, 2);   /* draw the ball */
+   }
+   glPopMatrix();
 
-   glEnd();
+   glPushMatrix();
+   {
+      setFontSize(0.11);
+
+      glColor3f(1.0, 1.0, 0.0);
+      printTextAt(20.0, 3200.0, "Projectile Demo || Controls -> f - Forward, r - Reverse");
+
+      glColor3f(0.0, 1.0, 0.0);
+      char buffer[100];
+      float xSpeed = (float) VELOCITY_X;
+      float ySpeed = getSpeed();
+      float speed = sqrt(ySpeed * ySpeed + xSpeed * xSpeed);
+
+      sprintf(buffer, "Speed in X direction %.1f m/s", (float) VELOCITY_X);
+      printTextAt(20.0, 3000.0, buffer);
+      sprintf(buffer, "Speed in Y direction %.1f m/s", ySpeed);
+      printTextAt(20.0, 2800.0, buffer);
+      sprintf(buffer, "Speed %.1f m/s", speed) ;
+      printTextAt(20.0, 2600.0, buffer);
+
+   }
+   glPopMatrix();
 
    glFlush ();
 }
